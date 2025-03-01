3280f2eb4593a883c7303693d4b29b11ae2675ed
marioSim2002 <wajd.simaan@gmail.com>
2025-03-01 15:50:36 +0200

    Hover effect

--- src/main/java/com/example/solvesphereadmins/Controllers/MainDashboardController.java
+++ src/main/java/com/example/solvesphereadmins/Controllers/MainDashboardController.java
@@ -2,18 +2,53 @@ package com.example.solvesphereadmins.Controllers;
 
 import com.example.solvesphereadmins.AdminUnit.Admin;
 import javafx.fxml.FXML;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+import java.util.List;
 
 public class MainDashboardController {
     Admin currentAdmin;
-    @FXML
-    private StackPane manageAdmins;
+
+    @FXML private StackPane manageAdmins;
+    @FXML private StackPane manageUsers;
+    @FXML private StackPane managePosts;
+    @FXML private StackPane manageComments;
+
 
 
     public void init(Admin currentAdmin){
         this.currentAdmin = currentAdmin;
+        List<StackPane> cards = List.of(manageAdmins, manageUsers, managePosts, manageComments);
+
+        // Shadow effect
+        DropShadow shadowEffect = new DropShadow();
+        shadowEffect.setColor(Color.web("#3498db")); // Blue shadow
+        shadowEffect.setRadius(10);
+        shadowEffect.setSpread(0.2);
+
+        // Apply hover effect to each card
+        for (StackPane card : cards) {
+            card.setOnMouseEntered(event -> applyHoverEffect(card, shadowEffect));
+            card.setOnMouseExited(event -> removeHoverEffect(card));
+        }
     }
+
+    private void applyHoverEffect(StackPane card, DropShadow shadowEffect) {
+        card.setEffect(shadowEffect);
+        card.setScaleX(1.05);
+        card.setScaleY(1.05);
+    }
+
+    private void removeHoverEffect(StackPane card) {
+        card.setEffect(null);
+        card.setScaleX(1.0);
+        card.setScaleY(1.0);
+    }
+
     public void handleManageAdmins() {
     }
 
