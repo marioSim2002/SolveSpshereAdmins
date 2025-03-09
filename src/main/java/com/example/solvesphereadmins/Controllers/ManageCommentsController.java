e30e3b6273a6d94d8a6ba6db5522e6ddb2e0f37e
marioSim2002 <wajd.simaan@gmail.com>
2025-03-09 17:19:34 +0200

    comment filtering

--- src/main/java/com/example/solvesphereadmins/Controllers/ManageCommentsController.java
+++ src/main/java/com/example/solvesphereadmins/Controllers/ManageCommentsController.java
@@ -12,6 +12,7 @@ import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,6 +20,9 @@ public class ManageCommentsController {
     @FXML private ListView<HBox> commentsListView;
     @FXML private TextField searchField;
     @FXML private Label totalCommentsLabel;
+    @FXML private ComboBox<String> sortComboBox;
+    @FXML private CheckBox solutionFilterCheckBox;
+
 
     private final CommentDAO commentDAO = new CommentDAOImpl();
     private ObservableList<Comment> allComments = FXCollections.observableArrayList();
@@ -26,6 +30,10 @@ public class ManageCommentsController {
     @FXML
     public void initialize() {
         loadComments();
+        sortComboBox.setItems(FXCollections.observableArrayList("Most Upvoted", "Least Upvoted"));
+        sortComboBox.setValue("Most Upvoted"); // default
+        // even listener for sorting
+        sortComboBox.setOnAction(event -> handleSort());
     }
 
     private void loadComments() {
@@ -65,6 +73,52 @@ public class ManageCommentsController {
 
     public void refreshCommentList() {loadComments();}
 
+
+    @FXML
+    private void handleSort() {
+        String selectedOption = sortComboBox.getValue();
+        if (selectedOption != null) {
+            List<Comment> sortedList = new ArrayList<>(allComments);
+            sortedList.sort((c1, c2) -> {
+                if ("Most Upvoted".equals(selectedOption)) {
+                    return Integer.compare(c2.getUpvotes(), c1.getUpvotes()); // Descending
+                } else {
+                    return Integer.compare(c1.getUpvotes(), c2.getUpvotes()); // Ascending
+                }
+            });
+            updateCommentsList(sortedList);
+        }
+    }
+
+    @FXML
+    private void handleFilterSolutions() {
+        boolean filterSolutions = solutionFilterCheckBox.isSelected();
+        List<Comment> filteredList = allComments.stream()
+                .filter(comment -> !filterSolutions || comment.isSolution()) //checked, filter solutions
+                .collect(Collectors.toList());
+        updateCommentsList(filteredList);
+    }
+
+
+    private void updateCommentsList(List<Comment> comments) {
+        ObservableList<HBox> commentItems = FXCollections.observableArrayList();
+
+        for (Comment comment : comments) {
+            try {
+                FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/solvesphereadmins/CommentItem.fxml"));
+                HBox commentItem = loader.load();
+
+                CommentItemController controller = loader.getController();
+                controller.setComment(comment, this); // Set the comment and pass the controller
+
+                commentItems.add(commentItem);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        commentsListView.setItems(commentItems);
+    }
 
     @FXML
     private void handleClose() {
